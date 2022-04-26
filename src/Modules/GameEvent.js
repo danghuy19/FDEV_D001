54f77e4c8bf10571b11e093a4d173bfc89fdb5db
Hung Nguyen <nxhung118@gmail.com>
2022-04-26 20:27:24 +0700

    finished basic controller

--- src/Modules/GameEvent.js
+++ src/Modules/GameEvent.js
@@ -7,8 +7,14 @@ class GameEvent extends Component {
         super(props);
         this.state = {
             speed: 1,
-            position_road: 0
+            position_road: 0,
+            time_run: 0,
+            score: 0,
+            player_position_x: 0,
+            player_position_y: 0
         }
+
+        this.handleController = this.handleController.bind(this);
     }
 
     componentDidMount(){
@@ -16,10 +22,48 @@ class GameEvent extends Component {
     }
 
     run_game = () => {
+        //console.log(this.state.speed);
         this.setState({
-            speed: 1,
-            position_road: this.state.position_road + this.state.speed
+            speed: 1 + Math.ceil(this.state.time_run / 5000),
+            position_road: this.state.position_road + this.state.speed,
+            time_run: this.state.time_run + 40,
+            score: this.state.score + this.state.speed,
+            player_position_x: this.state.player_position_x,
+            player_position_y: this.state.player_position_y
         });
+    }
+
+    handleController = (e) => {
+        console.log(e.keyCode);
+
+        if(e.keyCode == 37) //trai
+        {
+            this.setState(prevState => {
+                prevState.player_position_x -= 1;
+                return prevState;
+            });
+        }
+        else if(e.keyCode == 38) // tren
+        {
+            this.setState(prevState => {
+                prevState.player_position_y += 1;
+                return prevState;
+            });
+        }
+        else if(e.keyCode == 39) //phai
+        {
+            this.setState(prevState => {
+                prevState.player_position_x += 1;
+                return prevState;
+            });
+        }
+        else if(e.keyCode == 40) // duoi
+        {
+            this.setState(prevState => {
+                prevState.player_position_y -= 1;
+                return prevState;
+            });
+        }
     }
 
     render() {
@@ -28,8 +72,13 @@ class GameEvent extends Component {
                 <div className="game_container" style={{
                     backgroundPosition: '0 ' + this.state.position_road + 'px'
                 }}>
-                    <div className="player"></div>
+                    <div className="score">{this.state.score}</div>
+                    <div className="player" style={{
+                        left: this.state.player_position_x,
+                        bottom: this.state.player_position_y
+                    }}></div>
                 </div>
+                <input type="text" className="dieu_khien" id="dieu_khien" onKeyDown={this.handleController} />
             </div>
         );
     }
