445a4996368eff647d05902e3ff1a792954b5142
Hung Nguyen <nxhung118@gmail.com>
2022-05-10 20:49:52 +0700

    finished route page and defined group project

--- src/Widgets/Content.js
+++ src/Widgets/Content.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
-import LastestGame from '../Modules/LastestGame';
-import ListGame from '../Modules/ListGame';
-import Poster from '../Modules/Poster';
-import Xbox from '../Modules/Xbox';
 import Cart from '../Modules/Cart';
-import FormCart from '../Modules/FormCart';
-import { confirmAlert } from 'react-confirm-alert';
+import GioHang from '../Pages/GioHang';
+
+import TrangChu from "../Pages/TrangChu";
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import RandomGroup from '../Pages/RandomGroup';
 
 
 class Content extends Component {
+
     constructor(props) {
         super(props);
         this.state = {
@@ -16,112 +17,21 @@ class Content extends Component {
         }
     }
 
-    addToCart = (item) => {
-        //console.log(item);
-        let mang = this.state.mang_gio_hang;
-        if(mang){
-            let flag = 0;
-            for(var i = 0; i < mang.length; i++){
-                if(mang[i].id == item.id){
-                    mang[i].quantity += 1;
-                    flag = 1;
-                }
-            }
-            if(flag == 0){
-                item.quantity = 1;
-                mang.push(item);
-                
-            }
-        }
-        else {
-            item.quantity = 1;
-            mang.push(item);
-        }
-
-        this.setState(prevState => {
-            prevState.mang_gio_hang = mang;
-            return prevState;
-        })
-    }
-
-    DescreaseItemCart = (item) => {
-        let mang = this.state.mang_gio_hang;
-        for(var i = 0; i < mang.length; i++){
-            if(mang[i].id == item.id){
-                if(mang[i].quantity > 1){
-                    mang[i].quantity -= 1;
-                }
-                else{
-                    //do nothing
-                }
-            }
-        }
-
-        this.setState(prevState => {
-            prevState.mang_gio_hang = mang;
-            return prevState;
-        })
-    }
-
-    RemoveItemCart = (item) => {
-
-        const options = {
-            title: 'Confirm',
-            message: 'Bạn chắc chắn muốn xoá item này khỏi giỏ hàng hay không?',
-            buttons: [
-              {
-                label: 'Yes',
-                onClick: () => {
-                    let mang = this.state.mang_gio_hang;
-
-                    for(var i = 0; i < mang.length; i++){
-                        if(mang[i].id == item.id){
-                            mang.splice(i, 1);
-                        }
-                    }
-            
-                    //console.log(mang);
-                    this.setState(prevState => {
-                        prevState.mang_gio_hang = mang;
-                        return prevState;
-                    });
-                }
-              },
-              {
-                label: 'No',
-                onClick: () => {
-                    //do nothing
-                }
-              }
-            ],
-            childrenElement: () => <div />,
-            closeOnEscape: true,
-            closeOnClickOutside: true,
-            keyCodeForClose: [8, 32],
-            willUnmount: () => {},
-            afterClose: () => {},
-            onClickOutside: () => {},
-            onKeypressEscape: () => {},
-            overlayClassName: "overlay-custom-class-name"
-        };
-          
-        confirmAlert(options);
-        
-    }
+    
 
     render() {
         return (
             <div>
-                <ListGame handleaddToCart={this.addToCart} />
-                <FormCart CartItems={this.state.mang_gio_hang} 
-                handleAddToCart={this.addToCart}
-                handleDescreaseItemCart={this.DescreaseItemCart} 
-                handleRemoveItemCart={this.RemoveItemCart} />
-                <LastestGame />
-                <Poster />
-                <Xbox />
-                <Cart CartItems={this.state.mang_gio_hang}  />
                 
+                <Router>
+                    <Routes>
+                        <Route path="/" element={<TrangChu />} />
+                        <Route path="/gio-hang" element={<GioHang />} />
+                        <Route path="/random" element={<RandomGroup />} />
+                    </Routes>
+                </Router>
+                
+                <Cart CartItems={this.state.mang_gio_hang}  />
             </div>
         );
     }
